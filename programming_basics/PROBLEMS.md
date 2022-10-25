9deb2ca805a3cd04b2bbf66b7b0e95675883785d
Flu3nt <gnedyalkov94@gmail.com>
2022-10-25 10:00:03 +0300

    Write problem description. Solve first two problems.

---
+++ programming_basics/PROBLEMS.md
@@ -0,0 +1,7 @@
+# First steps in programming
+
+1. Problem 1: Hello, Softuni
+
+Write a function that prints the text "Hello, Softuni"
+
+2. Write a function that prints the numbers from 1 to 10.
---
+++ programming_basics/first_steps.js
@@ -0,0 +1,13 @@
+function printHelloSoftUni() {
+  console.log("Hello SoftUni")
+}
+
+printHelloSoftUni()
+
+function printOneToTen() {
+  for (let i = 1; i <= 10; i++) {
+    console.log(i)
+  }
+}
+
+printOneToTen()
