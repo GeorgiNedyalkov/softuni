70c16a07dc1efc5edd01573185d808598d798bad
Flu3nt <gnedyalkov94@gmail.com>
2022-10-25 13:48:35 +0300

    Solve all problems

--- programming_basics/PROBLEMS.md
+++ programming_basics/PROBLEMS.md
@@ -4,4 +4,34 @@
 
 Write a function that prints the text "Hello, Softuni"
 
-2. Write a function that prints the numbers from 1 to 10.
+2. Problem 2: Print 1 to 10
+
+Write a function that prints the numbers from 1 to 10.
+
+3. Calculate the area of a square
+
+Write a function that receives a number the side of a square and return the area of the square
+
+4. Inches to centimeters conveter
+
+Write a function that receives a number and coverts it from inches to centimeters.
+( 1 inch = 2.54 cm)
+
+5. Greet by name
+
+Write a function that receives a string (name) and greets the person
+
+6. Concatenate Data
+
+Write a function that takes a first name, last name, age and town of a person and print a statement.
+
+7. Project creation
+
+Calculate how much time will it take an architect to create the projects for several real estate developments. One project takes 3 hours.
+
+8. Pet shop
+
+Write a programm that calc the necessary expenses for the purchase of cats and dogs.
+Dog food is 2.50 bgn and cat food is 4 bgn.
+
+9. Yard Greens
--- programming_basics/first_steps.js
+++ programming_basics/first_steps.js
@@ -11,3 +11,76 @@ function printOneToTen() {
 }
 
 printOneToTen()
+
+function calcSquareArea(input) {
+  let num = Number(input)
+  let area = num * num
+  console.log(area)
+}
+
+calcSquareArea("5")
+
+function convertInchesToCentimeters(input) {
+  let inches = +input
+  let centimeters = 2.54 * inches
+  console.log(centimeters)
+}
+
+convertInchesToCentimeters("5")
+
+function greetPerson(name) {
+  console.log(`Hello, ${name}!`)
+}
+
+greetPerson("Georgi")
+
+function concatenateData(input) {
+  let firstName = input[0]
+  let lastName = input[1]
+  let age = +input[2]
+  let town = input[3]
+
+  console.log(
+    `You are ${firstName} ${lastName}, a ${age}-years old person from ${town}`
+  )
+}
+
+concatenateData(["Maria", "Ivanova", 20, "Sofia"])
+
+function calcWorkHours(input) {
+  let name = input[0]
+  let numOfProjects = +input[1]
+  let totalWorkHours = numOfProjects * 3
+
+  console.log(
+    `The architect ${name} will need ${totalWorkHours} hours to complete ${numOfProjects} project/s.`
+  )
+}
+
+calcWorkHours(["Georgi", "4"])
+
+function calcExpenses(input) {
+  let dogFood = Number(input[0]) * 2.5
+  let catFood = Number(input[1]) * 4
+  let totalExpenses = dogFood + catFood
+
+  console.log(totalExpenses + " lv.")
+}
+
+calcExpenses(["13", "9"])
+
+function calcYardExpenses(input) {
+  let squareMeters = +input[0]
+  const priceSqM = 7.61
+  const discountPercent = 0.18
+  let grossPrice = squareMeters * priceSqM
+
+  let discountAmount = discountPercent * grossPrice
+
+  let discountedPrice = grossPrice - discountAmount
+
+  console.log(`The final price is: ${discountedPrice} lv.`)
+  console.log(`The discount is: ${discountAmount} lv.`)
+}
+
+calcYardExpenses(["150"])
